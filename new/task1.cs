31c7271bbbd113c19fa26102a150efcad56e099b
Thilagavathi <thilagavathivijayan850@gmail.com>
2022-06-06 20:35:50 +0530

    almost completed

---
+++ new/task1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int opt;
+        string ch;
+        Console.WriteLine ("****WELCOME TO QUIZ APP*****");
+        Console.WriteLine ("*GENERAL INSTRUCTIONS*");
+        Console.WriteLine ("1.You have two chances to enter the correct answe");
+        Console.WriteLine ("1.General knowledge\n2.English\n3.Maths");
+        Console.WriteLine ("Enter your interested field:");
+        opt=int.Parse(Console.ReadLine());
+        switch(opt){
+            case 1:
+            Console.Write ("1)Who is the fatheroounation?/na.Gandhiji/nb.Nehru/nc.subash chadhrabose");
+             ch=Console.ReadLine ();
+            if(ch == a)
+                Console.Write ("/nYes!You Are Right");
+            
+            else
+            for(int i=1;i<3;i++)
+            ch=Console.ReadLine ();
+        
+        }
+        }
+        }
